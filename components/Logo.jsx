a850899df2c6bb71da0544efb5d511dda2c7f600
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-02-07 23:24:34 +0500

    logo colors

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -10,8 +10,9 @@ import { useRouter } from 'next/navigation';
 export const Logo = ({ open }) => {
   return (
      <Canvas className={`absolute top-0 left-0 w-screen ${open ? "z-50" : "z-0"} h-screen  pointer-events-none`}>
-      <Environment preset="studio" />
-      <ambientLight intensity={1} />
+      <Environment preset="city" />
+      <ambientLight intensity={1}/>
+      
       <Model position={[0, 2, 0]} rotation={[Math.PI / 2, 0, 0]} scale={10} open={open} />
     </Canvas>
   );
@@ -20,6 +21,7 @@ export function Model({open, ...props }) {
   const { nodes, materials } = useGLTF('/logo.gltf')
   const group = useRef();
   const router = useRouter();
+  console.log(materials)
 
   const tl = gsap.timeline();
   const top = useRef();
@@ -37,12 +39,13 @@ export function Model({open, ...props }) {
         .to(logoRef.current.position,{duration: 2, y: 2, onComplete: () => router.push('/about')},2);
     }
   }, [open]);
-  const gold = new THREE.MeshPhysicalMaterial({color: '#FDD017', metalness: 0.8, roughness: 0.2})
-  const silver = new THREE.MeshPhysicalMaterial({color: 'lightgray', metalness: 0.9, roughness: 0.2})
+  const gold = new THREE.MeshPhysicalMaterial({color: '#F3C749', metalness: 0.8, roughness: 0.2})
+  const silver = new THREE.MeshPhysicalMaterial({color: '#9A9A9A', metalness: 0.9, roughness: 0.2})
   return (
     <group {...props} dispose={null} ref={group}>
       <mesh geometry={nodes.top.geometry} ref={top} material={gold} position={[-0.25, -0.034, 0.115]} />
       <mesh geometry={nodes.bottom.geometry} ref={bottom} material={silver} position={[-0.25, -0.034, 0.115]} />
+      <pointLight intensity={6}/>
       <group ref={logoRef}>
         <mesh geometry={nodes.xvi.geometry} material={silver} position={[-0.25, -0.034, 0.115]} />
         <mesh geometry={nodes.trading.geometry} material={gold} position={[-0.25, -0.034, 0.115]} />
