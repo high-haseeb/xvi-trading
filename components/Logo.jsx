2f8c42e17918967c02e0e79382b8b5af2be27b38
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-02-07 23:34:22 +0500

    updated some things

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useRef, useLayoutEffect, useEffect } from 'react'
 import { useGLTF, Environment } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber';
+import { Canvas  } from '@react-three/fiber';
 import gsap from 'gsap';
 import * as THREE from 'three'
 import { useRouter } from 'next/navigation';
@@ -9,19 +9,18 @@ import { useRouter } from 'next/navigation';
 
 export const Logo = ({ open }) => {
   return (
-     <Canvas className={`absolute top-0 left-0 w-screen ${open ? "z-50" : "z-0"} h-screen  pointer-events-none`}>
-      <Environment preset="city" />
-      <ambientLight intensity={1}/>
-      
+    <Canvas className={`absolute top-0 left-0 w-screen ${open ? "z-50" : "z-0"} h-screen  pointer-events-none`}>
+      <ambientLight intensity={3} />
+      <directionalLight intensity={2} position={[0, 10, 0]} />
+      <directionalLight intensity={2} position={[0,-10, 0]} />
       <Model position={[0, 2, 0]} rotation={[Math.PI / 2, 0, 0]} scale={10} open={open} />
     </Canvas>
   );
 };
-export function Model({open, ...props }) {
+export function Model({ open, ...props }) {
   const { nodes, materials } = useGLTF('/logo.gltf')
   const group = useRef();
   const router = useRouter();
-  console.log(materials)
 
   const tl = gsap.timeline();
   const top = useRef();
@@ -29,23 +28,23 @@ export function Model({open, ...props }) {
   const logoRef = useRef();
 
   useEffect(() => {
-    if(open){
-      tl.to(group.current.scale,    {duration: 2, x: 5,y: 5,z: 5},0)
-        .to(group.current.position, {duration: 2, x: 0,y: 0,z: 3},0)
-        .to(top.current.rotation,   {duration: 2, x: -Math.PI/2 },2)
-        .to(top.current.position,   {duration: 2, y: 1.5, z: -0.1 },2)
-        .to(bottom.current.rotation,{duration: 2, x: Math.PI/2  },2)
-        .to(bottom.current.position,{duration: 2, y: 1}, 2)
-        .to(logoRef.current.position,{duration: 2, y: 2, onComplete: () => router.push('/about')},2);
+    if (open) {
+      tl.to(group.current.scale, { duration: 2, x: 5, y: 5, z: 5 }, 0)
+        .to(group.current.position, { duration: 2, x: 0, y: 0, z: 3 }, 0)
+        .to(top.current.rotation, { duration: 2, x: -Math.PI / 2 }, 2)
+        .to(top.current.position, { duration: 2, y: 1.5, z: -0.1 }, 2)
+        .to(bottom.current.rotation, { duration: 2, x: Math.PI / 2 }, 2)
+        .to(bottom.current.position, { duration: 2, y: 1 }, 2)
+        .to(logoRef.current.position, { duration: 2, y: 2, onComplete: () => router.push('/about') }, 2);
     }
   }, [open]);
-  const gold = new THREE.MeshPhysicalMaterial({color: '#F3C749', metalness: 0.8, roughness: 0.2})
-  const silver = new THREE.MeshPhysicalMaterial({color: '#9A9A9A', metalness: 0.9, roughness: 0.2})
+  const gold = new THREE.MeshPhysicalMaterial({ color: '#F3C749', metalness: 0.8, roughness: 0.2 })
+  const silver = new THREE.MeshPhysicalMaterial({ color: '#9A9A9A', metalness: 0.9, roughness: 0.2 })
   return (
     <group {...props} dispose={null} ref={group}>
       <mesh geometry={nodes.top.geometry} ref={top} material={gold} position={[-0.25, -0.034, 0.115]} />
       <mesh geometry={nodes.bottom.geometry} ref={bottom} material={silver} position={[-0.25, -0.034, 0.115]} />
-      <pointLight intensity={6}/>
+      <pointLight intensity={6} />
       <group ref={logoRef}>
         <mesh geometry={nodes.xvi.geometry} material={silver} position={[-0.25, -0.034, 0.115]} />
         <mesh geometry={nodes.trading.geometry} material={gold} position={[-0.25, -0.034, 0.115]} />
@@ -55,3 +54,4 @@ export function Model({open, ...props }) {
 }
 
 useGLTF.preload('/logo.gltf')
+
